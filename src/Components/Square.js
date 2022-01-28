492e881616b5a4dd2a39734661d161fc577aadde
progri27 <progri27@gmail.com>
2022-01-28 14:44:39 +0100

    game

---
+++ src/Components/Square.js
@@ -0,0 +1,10 @@
+// creates a square
+
+import "../index.css";
+export default function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
