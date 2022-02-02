3dc49eac07819b9c8d09a4a65b3cc478eb5026ef
progri27 <progri27@gmail.com>
2022-02-02 13:37:23 +0100

    game

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -88,16 +88,24 @@ export default class Game extends React.Component {
     }
 
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
+      <div className="page-wrapper">
+        <div className="header">
+          <h1>Tic Tac Toe</h1>
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+        <div className="game">
+          <div className="game-board">
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
+        </div>
+        <div className="footer">
+          <p>footer</p>
         </div>
       </div>
     );
